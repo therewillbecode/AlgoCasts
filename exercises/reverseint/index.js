5c02bc89d232fbbb0cd619f057a665e1de3e706c
therewillbecode <tomw08@gmail.com>
2020-09-26 16:57:34 +0100

    ReverseInt completed

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,14 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+const reverseInt = n =>
+      parseInt(
+        Math.sign(n) *
+        Math.abs(n)
+        .toString()
+        .split('')
+        .reverse()
+        .join('')
+             )
 
 module.exports = reverseInt;
