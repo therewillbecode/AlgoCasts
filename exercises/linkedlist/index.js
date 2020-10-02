8ebe87204e600c14c2e48afdf54ddec92b7f1ad6
therewillbecode <tomw08@gmail.com>
2020-10-02 08:50:12 +0100

    Size linkedlist

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,8 +2,34 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+    constructor(data, next = null){
+        this.data = data
+        this.next = next
+    }
+}
 
-class LinkedList {}
+class LinkedList {
+    constructor() {
+        this.head = null
+    }
+
+    insertFirst (data) {
+        this.head = new Node(data, this.head)
+    }
+
+    size () {
+        let counter = 0
+
+        while (this.head) {
+           this.head = this.head.next
+           counter++
+        }
+
+        return counter
+    }
+}
+
+const list = new LinkedList()
 
 module.exports = { Node, LinkedList };
