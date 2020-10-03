7a2d4d975b3bf2416659bf12de96c16f4b725248
therewillbecode <tomw08@gmail.com>
2020-10-03 20:26:02 +0100

    Validate BST

--- exercises/validate/index.js
+++ exercises/validate/index.js
@@ -5,6 +5,33 @@
 // every node's right hand child is greater than
 // the parent
 
-function validate(node, min = null, max = null) {}
+const validate = (node, min = null, max = null) => {
+    // if(node.right && node.right.data < min) return false
+    // if(node.left && node.left.data > max) return false
+
+    if(max !== null && node.data > max) {
+        return false
+    }
+
+
+    if(min !== null && node.data < min) {
+        return false
+    }
+
+    if(node.val < min || node.val > max) return false
+
+
+    if (node.left && node.right) {
+        return validate(node.left, min, node.data) && validate(node.right, node.data, max)
+    }
+    else if(node.left) {
+        return validate(node.left, min, node.data)
+    }
+    else if(node.right) {
+        return validate(node.right, node.data, max)
+    }
+
+    return true
+}
 
 module.exports = validate;
