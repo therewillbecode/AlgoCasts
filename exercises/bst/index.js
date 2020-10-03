b4f299c9fffc9da423ce4829bace398ce7b0117f
therewillbecode <tomw08@gmail.com>
2020-10-03 09:54:03 +0100

    Insert into BST

--- exercises/bst/index.js
+++ exercises/bst/index.js
@@ -11,6 +11,31 @@
 // class.  Contains should accept a 'data' argument
 // and return the Node in the tree with the same value.
 
-class Node {}
+class Node {
+    constructor(data) {
+        this.data = data
+        this.left = null
+        this.right = null
+    }
+
+    insert(data){
+        if (this.left && this.data > data) {
+            this.left.insert(data)
+        }
+
+        if (this.right && this.data < data) {
+            this.right.insert(data)
+        }
+
+        if (!this.right && this.data < data) {
+            this.right = new Node(data)
+        }
+
+        if (!this.left && this.data > data) {
+            this.left = new Node(data)
+        }
+
+    }
+}
 
 module.exports = Node;
